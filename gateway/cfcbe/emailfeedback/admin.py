284b2faea0213859e771a1e80f7a79414f8c8b25
nelsonadagi <thinkiqbs@gmail.com>
2025-05-30 04:23:07 +0300

    remove audio files

--- gateway/cfcbe/emailfeedback/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Email
-
-@admin.register(Email)
-class EmailAdmin(admin.ModelAdmin):
-    list_display = ('subject', 'sender', 'recipient', 'received_date', 'is_read')
-
--- gateway/cfcbe/feedback/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from django.conf import settings
-
-def pytest_configure(config):
-    """Modify INSTALLED_APPS before test collection."""
-    if 'feedback' not in settings.INSTALLED_APPS:
-        settings.INSTALLED_APPS = list(settings.INSTALLED_APPS) + ['feedback']
