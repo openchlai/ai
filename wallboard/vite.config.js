c2763c8177138df489f440f230bb5bd16d97f738
nelsonadagi <thinkiqbs@gmail.com>
2025-09-24 21:12:01 +0300

    fix merge conflict

--- wallboard/vite.config.js
+++ wallboard/vite.config.js
@@ -4,13 +4,32 @@ import path from 'path'
 
 export default defineConfig({
     plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
-        }
-    },
     server: {
         port: 3000,
-        open: true
-    }
-})
+        host: '0.0.0.0',
+        open: true,
+        hmr: true,
+        proxy: {
+            '/api-proxy': {
+                target: 'https://demo-openchs.bitz-itc.com/helpline',
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/api-proxy/, '/helpline'),
+                configure: (proxy, options) => {
+                    proxy.on('proxyReq', (_proxyReq, req) => {
+                        console.log(`[proxy] ${req.method} ${req.url} -> ${proxy.target}${req.url}`);
+                    });
+                },
+            },
+        },
+    },
+    build: {
+        sourcemap: true,
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+});
+
